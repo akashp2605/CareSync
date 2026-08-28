e6081e9519af93f1c41840c3e3602d4add9d6f25
Akash <akashp2605@gmail.com>
2026-08-28 11:44:32 +0530

    Added allowedorigins

--- backend/src/main/java/com/hospital/config/CorsConfig.java
+++ backend/src/main/java/com/hospital/config/CorsConfig.java
@@ -7,13 +7,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://careesync.vercel.app/"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
